b7b4cea15136c16c73d29a2eab6a3edb8de5f1ee
kevin <42000745+kevin01yaya@users.noreply.github.com>
2020-07-24 20:44:21 +0800

    20200724

--- src/main/serial_ex.py
+++ src/main/serial_ex.py
@@ -1,9 +1,9 @@
 import serial
-ser = serial.Serial('COM10', 9600, timeout=1)
+serialProtocol = serial.Serial('COM10', 9600, timeout=1)
 
 while 1:
-    s = ser.readline()
-    sDecode = s.decode('utf-8')
-    sArray = sDecode.split()
-    print(sArray)
+    serialRead = serialProtocol.readline()
+    serialDecode = serialRead.decode('utf-8')
+    serialArray = serialDecode.split()
+    print(serialArray)
 
